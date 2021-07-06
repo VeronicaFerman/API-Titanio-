e9b7a892db0b713eca1fa3442fd71c7e2a1c3480
Francisco Alejandro Murillo Oviedo <murillo.francisco2001@gmail.com>
2021-07-06 16:27:20 -0600

    delete unnecesary stuff 2.0

--- web/routes/main_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-
-
-class MainRoutes:
-    @staticmethod
-    def configure_routes(app):
-        @app.route("/")
-        def home():
-            return render_template("index.html")

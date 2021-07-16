4e6bbef304c53053f4934b748f327dd383bb0ba4
VeronicaFerman <65994364+VeronicaFerman@users.noreply.github.com>
2021-07-16 09:33:53 -0600

    result of get and post on consume.py/final fixes for post

--- rest/consume_api.py
+++ rest/consume_api.py
@@ -1,17 +1,21 @@
 import requests
 
-response = requests.get("http://localhost:23512/tour/Chalatenango")
+"""response = requests.get("http://localhost:23512/tour/San Salvador")
 print(response)
 if response.status_code == 200:
     dataJson = response.json()
-    print(dataJson)
+    print(dataJson)"""
+#Result from GET: {'id': 2, 'nombre': 'Iglesia El Rosario', 'ubicacion': 'San Salvador', 'imagen': 'https://media-cdn.tripadvisor.com/media/photo-s/0d/41/a8/8e/interior-de-la-iglesia.jpg'}
 
-"""response = requests.post("http://localhost:23512/tour/0")
+"""response = requests.post("http://localhost:23512/tour/all")
 print(response)
 if response.status_code == 200:
     dataJson = response.json()
-    print(dataJson)
-"""
+    print(dataJson)"""
+#Result fron POST: [{'id': 1, 'nombre': 'Playa El Tunco', 'ubicacion': 'La Libertad', 'imagen': 'https://ipanelsalvador.files.wordpress.com/2012/09/eltiunco.jpg?w=590'}, ...
+# {'id': 20, 'nombre': 'Volcan De Santa Ana O Ilamatepec', 'ubicacion': 'Santa Ana', 'imagen': 'https://astelus.com/wp-content/viajes/volcan-ilamatepec-el-salvador.jpg'}]
+
+
 """data = {
     "nombre": "Laguna de Alegría",
     "ubicacion": "San Vicente"
